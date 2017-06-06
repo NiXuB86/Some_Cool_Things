2e3bca352de40d4aeee2c5ad584061ad0c191372
NiXuB86 <eg.eremeeff2012@yandex.ru>
2017-06-06 07:51:32 +0300

    Fixing the name of gas

--- src/main/java/org/nixub86/SCT/Gases/GasesSCT.java
+++ src/main/java/org/nixub86/SCT/Gases/GasesSCT.java
@@ -8,7 +8,7 @@ import glenn.gasesframework.api.GFAPI;
 import glenn.gasesframework.api.gastype.GasType;
 
 public class GasesSCT {
-	public static final GasType gasTypeNeurotoxin = new GasTypeNeurotoxin(true, 8, "nitrousGas", 0x6F3F2F7F, 3, 4, Combustibility.NONE).setEffectRate(EffectType.BLINDNESS, 1).setEffectRate(EffectType.SUFFOCATION, 2).setEffectRate(EffectType.SLOWNESS, 16).setCreativeTab(ItemsSCT.SCTTab);
+	public static final GasType gasTypeNeurotoxin = new GasTypeNeurotoxin(true, 8, "Neurotoxin", 0x6F3F2F7F, 3, 4, Combustibility.NONE).setEffectRate(EffectType.BLINDNESS, 1).setEffectRate(EffectType.SUFFOCATION, 2).setEffectRate(EffectType.SLOWNESS, 16).setCreativeTab(ItemsSCT.SCTTab);
 	
 	public static final void preInit() {
 		GFAPI.registry.registerGasType(gasTypeNeurotoxin);
