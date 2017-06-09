4f606140540181b75a9f3adbcfc6b889dfff692c
NiXuB86 <NiXuB86@users.noreply.github.com>
2017-06-09 20:35:16 +0300

    Added README.md and now all art licensed under CC-BY-SA-4.0

---
+++ README.md
@@ -0,0 +1,3 @@
+ # Some Cool Things
+This mod for minecraft adds some things.
+Source code licensed under GPLv3(file LICENSE). Art licensed under CC-BY-4.0(file LICENSE - ART.txt.
--- src/main/java/org/nixub86/SCT/Items/Universal.java
+++ src/main/java/org/nixub86/SCT/Items/Universal.java
@@ -8,8 +8,11 @@ import com.google.common.collect.Sets;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import scala.swing.event.Key;
 
 public class Universal extends ItemPickaxe {
@@ -24,7 +27,20 @@ public class Universal extends ItemPickaxe {
 		this.setTextureName("sct:Universal");
 		this.setUnlocalizedName("Universal");
 		
-		plavka = true;
+		plavka = false;
 		
+	}
+	
+	@Override
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float px, float py, float pz) {
+		if(plavka=false){
+			plavka = true;
+		}
+		if(plavka=true){
+			plavka = false;
+		}
+		
+		
+		return true;
 	}
 }
