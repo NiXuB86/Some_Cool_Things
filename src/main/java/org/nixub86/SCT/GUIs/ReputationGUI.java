ba2f43f080192514f3a1ec62f81c2b69acc19149
NiXuB86 <NiXuB86@users.noreply.github.com>
2017-12-29 14:04:11 +0300

    Fixed Gui

--- src/main/java/org/nixub86/SCT/GUIs/ReputationGUI.java
+++ src/main/java/org/nixub86/SCT/GUIs/ReputationGUI.java
@@ -2,16 +2,26 @@ package org.nixub86.SCT.GUIs;
 
 import org.nixub86.SCT.CommandReportAndLike.Command;
 import org.nixub86.SCT.CommandReportAndLike.Info;
+import org.nixub86.SCT.CommandReportAndLike.PlayerReport_Like;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 
 public class ReputationGUI extends Gui {
 	
 	//int Reputation = Command.Like - Command.Report;
 	
-	String text = "Reputation: "+ Info.rep;
+	
+	public int rep;
+	
+	EntityPlayerMP player;
+	
+	String text = "Reputation: ";
 	String color = "FFFFFF";
 	
 	public ReputationGUI(Minecraft mc)
@@ -19,6 +29,16 @@ public class ReputationGUI extends Gui {
 		ScaledResolution scaled = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
 		int width = scaled.getScaledWidth();
 		int height = scaled.getScaledHeight();
+		
+		player =  MinecraftServer.getServer().getConfigurationManager().func_152612_a(mc.thePlayer.getDisplayName());
+		
+		PlayerReport_Like pro = PlayerReport_Like.get(player);
+		
+		rep = pro.getLike() - pro.getReport();
+		
+		//System.out.println("Reputation: " + rep);
+		
+		text = text + rep;
 		
 		if (Info.rep >= 5)
 		{
